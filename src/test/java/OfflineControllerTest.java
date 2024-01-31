1851e2b3b8e34c6ca32d0f37d6c7cd7aad2dcdae
Baimanjvn <baimanjvn666@outlook.com>
2024-01-31 11:09:34 +0800

    完善OfflineController类

---
+++ src/test/java/OfflineControllerTest.java
@@ -0,0 +1,31 @@
+import cn.goindog.OpenMLauncher.account.Microsoft.MicrosoftController;
+import cn.goindog.OpenMLauncher.account.Microsoft.MicrosoftOAuthCodeMethod;
+import cn.goindog.OpenMLauncher.account.Microsoft.MicrosoftOAuthOptions;
+import cn.goindog.OpenMLauncher.account.Microsoft.MicrosoftOAuthType;
+import cn.goindog.OpenMLauncher.account.Offline.OfflineController;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class OfflineControllerTest {
+    public static void main(String[] args) {
+        MicrosoftOAuthOptions options = new MicrosoftOAuthOptions();
+        options.setOAuthType(MicrosoftOAuthType.AUTHORIZATION_CODE);
+        options.setMethod(new MicrosoftOAuthCodeMethod() {
+            @Override
+            public void GetMSCode(URI uri) {
+                try {
+                    java.awt.Desktop.getDesktop().browse(uri);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        MicrosoftController controller = new MicrosoftController();
+        controller.build(options);
+
+        OfflineController offlineController = new OfflineController();
+        offlineController.build("Richard Gates");
+    }
+}
