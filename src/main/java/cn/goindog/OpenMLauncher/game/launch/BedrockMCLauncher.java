6bcfebb8ba27f20e04ef7147502bc93591ef33b9
Richard-Gates <baimanjvn666@outlook.com>
2024-02-12 10:50:03 +0800

    加入基岩版启动器

--- src/main/java/cn/goindog/OpenMLauncher/game/launch/BedrockMCLauncher.java
+++ src/main/java/cn/goindog/OpenMLauncher/game/launch/BedrockMCLauncher.java
@@ -1,8 +1,5 @@
 package cn.goindog.OpenMLauncher.game.launch;
 
-import com.sun.jna.platform.win32.Advapi32Util;
-import com.sun.jna.platform.win32.WinReg;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
